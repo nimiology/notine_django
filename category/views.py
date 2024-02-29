7579d98122ea2ff1b4c16b2b62f3062f9f92d5dc
Nima <nimiologyy@gmail.com>
2024-02-29 13:44:40 +0330

    category delete

--- category/views.py
+++ category/views.py
@@ -1,12 +1,12 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
 
 from category.models import Category
 from category.serializers import CategorySerializer
 
 
-class CategoryViewSet(ReadOnlyModelViewSet, CreateAPIView):
+class CategoryViewSet(ReadOnlyModelViewSet, CreateAPIView, UpdateAPIView):
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated]
     filterset_fields = {
@@ -18,4 +18,7 @@ class CategoryViewSet(ReadOnlyModelViewSet, CreateAPIView):
         return Category.objects.filter(owner=self.request.user).order_by('-id')
 
     def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
+
+    def perform_update(self, serializer):
         return serializer.save(owner=self.request.user)
